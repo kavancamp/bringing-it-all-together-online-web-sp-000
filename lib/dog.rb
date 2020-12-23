6cd8605d2cd10b2396799e799dc9b097a141f526
Keenah Vancampenhout <kvancamp@mail.com>
2020-12-23 18:33:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,6 +2,7 @@ class Dog
 
   attr_accessor :name, :breed
     attr_reader :id
+    
   def initialize(id=nil, name, breed)
     @id = id
     @name = name
@@ -12,12 +13,12 @@ class Dog
       self.update
     else
       sql = <<-SQL
-        INSERT INTO dogs (name, breed)
-        VALUES (?, ?)
+        INSERT INTO dogs (name, breed) VALUES (?, ?)
       SQL
 
       DB[:conn].execute(sql, self.name, self.breed)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+      self.save
     end
   end
 
