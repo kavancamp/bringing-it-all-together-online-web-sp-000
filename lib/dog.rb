9aabc1c8d4c1fb3724504acf63b40623fc403ed8
Keenah Vancampenhout <kvancamp@mail.com>
2020-12-23 18:32:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -19,7 +19,7 @@ class Dog
       DB[:conn].execute(sql, self.name, self.breed)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
     end
-    save
+    dog
   end
 
   def self.create_table
