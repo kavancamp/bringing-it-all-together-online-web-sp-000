ee353ac2d2cd4617eb56e8658aca3745f7dbe84f
Keenah Vancampenhout <kvancamp@mail.com>
2020-12-23 17:49:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,13 +2,14 @@ class Dog
 
   attr_accessor :name, :breed
   attr_reader :id
-
+    @@all=[]
   def initialize(id=nil, name, breed)
     @id = id
     @name = name
     @breed = breed
+    @@all << save
   end
-
+  
   def self.create_table
     sql = <<-SQL
       CREATE TABLE IF NOT EXISTS dogs (
