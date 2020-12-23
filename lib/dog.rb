083d07d2c03ffd884bcf59334b40a44eb43f82d5
Keenah Vancampenhout <kvancamp@mail.com>
2020-12-23 18:31:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,27 @@
 class Dog
 
-  attr_accessor :id, :name, :breed
-
+  attr_accessor :name, :breed
+    attr_reader :id
   def initialize(id=nil, name, breed)
     @id = id
     @name = name
     @breed = breed
   end
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
+      SQL
 
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+      save
+    end
+  end
+  
   def self.create_table
     sql = <<-SQL
       CREATE TABLE IF NOT EXISTS dogs (
@@ -26,21 +40,6 @@ class Dog
       SQL
 
      DB[:conn].execute(sql)
-  end
-
-  def save
-    if self.id
-      self.update
-    else
-      sql = <<-SQL
-        INSERT INTO dogs (name, breed)
-        VALUES (?, ?)
-      SQL
-
-      DB[:conn].execute(sql, self.name, self.breed)
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-
-    end
   end
 
   def self.create(name:, breed:)
