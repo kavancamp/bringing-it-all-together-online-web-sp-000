be7dd082d9da7c7a77111fde1e90750afc4764db
Keenah Vancampenhout <kvancamp@mail.com>
2020-12-23 17:59:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,12 +2,12 @@ class Dog
 
   attr_accessor :name, :breed
   attr_reader :id
-    
+
   def initialize(id=nil, name, breed)
     @id = id
     @name = name
     @breed = breed
-  
+
   end
 
   def self.create_table
